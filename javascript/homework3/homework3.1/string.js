934a1891bf5cf7103ae1b5983edcd9d99f013958
simonailieva <simonailieva966@gmail.com>
2025-06-26 22:02:10 +0200

    Delete javascript directory

--- javascript/homework3/homework3.1/string.js
+++
@@ -1,9 +0,0 @@
-function type(input){
-    console.log (typeof(input));
-}
-type([]);
-type('Simona');
-type(false);
-type(11710);
-type(undefined);
-
--- javascript/homework3/homework3.2/script.js
+++
@@ -1,7 +0,0 @@
-function ConvertYears (){
-let years= parseInt(prompt("enter your age"));
-let DogYears  = HumanYears *7;
-let HumanYears = DogYears /7;
-console.log(`your age in dog years is ${DogYears}`);
-console.log(`your age in human years is ${HumanYears}`);
-}
